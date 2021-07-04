73c77efa954910d75cadb8c6fa62b8429cfa5d78
=APP-SISTEMAS <=adan.ordonez@plastinova.hn>
2021-07-04 11:54:44 -0600

    Cargado archivos a la rama

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace PruebagRID
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Agregar = new System.Windows.Forms.Button();
+            this.Columns = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rojoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.amarilloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.azulToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.verdeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ningunoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 64);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(940, 425);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.datagrid_DragDrop);
+            this.dataGridView1.DragOver += new System.Windows.Forms.DragEventHandler(this.datagrid_DragOver);
+            this.dataGridView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView1_KeyDown);
+            this.dataGridView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.datagrid_MouseDown);
+            this.dataGridView1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.datagrid_MouseMove);
+            // 
+            // Agregar
+            // 
+            this.Agregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Agregar.Location = new System.Drawing.Point(12, 9);
+            this.Agregar.Name = "Agregar";
+            this.Agregar.Size = new System.Drawing.Size(51, 37);
+            this.Agregar.TabIndex = 1;
+            this.Agregar.Text = "++";
+            this.Agregar.UseVisualStyleBackColor = true;
+            this.Agregar.Click += new System.EventHandler(this.Agregar_Click);
+            // 
+            // Columns
+            // 
+            this.Columns.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Columns.Location = new System.Drawing.Point(804, 9);
+            this.Columns.Name = "Columns";
+            this.Columns.Size = new System.Drawing.Size(148, 37);
+            this.Columns.TabIndex = 2;
+            this.Columns.Text = "Columns";
+            this.Columns.UseVisualStyleBackColor = true;
+            this.Columns.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(234, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Modo Edicion";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colorToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(104, 26);
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rojoToolStripMenuItem,
+            this.amarilloToolStripMenuItem,
+            this.azulToolStripMenuItem,
+            this.verdeToolStripMenuItem,
+            this.ningunoToolStripMenuItem});
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.colorToolStripMenuItem.Text = "Color";
+            // 
+            // rojoToolStripMenuItem
+            // 
+            this.rojoToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.rojoToolStripMenuItem.Name = "rojoToolStripMenuItem";
+            this.rojoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.rojoToolStripMenuItem.Text = "Rojo";
+            this.rojoToolStripMenuItem.Click += new System.EventHandler(this.rojoToolStripMenuItem_Click);
+            // 
+            // amarilloToolStripMenuItem
+            // 
+            this.amarilloToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.amarilloToolStripMenuItem.Name = "amarilloToolStripMenuItem";
+            this.amarilloToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.amarilloToolStripMenuItem.Text = "Amarillo";
+            this.amarilloToolStripMenuItem.Click += new System.EventHandler(this.amarilloToolStripMenuItem_Click);
+            // 
+            // azulToolStripMenuItem
+            // 
+            this.azulToolStripMenuItem.BackColor = System.Drawing.Color.DodgerBlue;
+            this.azulToolStripMenuItem.Name = "azulToolStripMenuItem";
+            this.azulToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.azulToolStripMenuItem.Text = "Azul";
+            this.azulToolStripMenuItem.Click += new System.EventHandler(this.azulToolStripMenuItem_Click);
+            // 
+            // verdeToolStripMenuItem
+            // 
+            this.verdeToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.verdeToolStripMenuItem.Name = "verdeToolStripMenuItem";
+            this.verdeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.verdeToolStripMenuItem.Text = "Verde";
+            // 
+            // ningunoToolStripMenuItem
+            // 
+            this.ningunoToolStripMenuItem.Name = "ningunoToolStripMenuItem";
+            this.ningunoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ningunoToolStripMenuItem.Text = "Ninguno";
+            this.ningunoToolStripMenuItem.Click += new System.EventHandler(this.ningunoToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(437, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 37);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Salir y Guardar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(986, 501);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Columns);
+            this.Controls.Add(this.Agregar);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Agregar;
+        private System.Windows.Forms.Button Columns;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rojoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem amarilloToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem azulToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem verdeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ningunoToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebagRID
+{
+    public partial class Form1 : Form
+    {
+        private Rectangle dragBoxFromMouseDown;
+        private int rowIndexFromMouseDown;
+        private int rowIndexOfItemUnderMouseToDrop;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void copiar_portapapeles(DataGridView datagrid)
+        {
+            DataObject objeto_datos = datagrid.GetClipboardContent();
+            Clipboard.SetDataObject(objeto_datos);
+        }
+        public void pegar_portapapeles(DataGridView datagrid)
+        {
+            try
+            {
+                string texto_copiado = Clipboard.GetText();
+                string[] lineas = texto_copiado.Split('\n');
+                int error = 0;
+                int fila = datagrid.CurrentCell.RowIndex;
+                int columna = datagrid.CurrentCell.ColumnIndex;
+                DataGridViewCell objeto_celda;
+
+                foreach (string linea in lineas)
+                {
+                    if (fila < datagrid.RowCount && linea.Length > 0)
+                    {
+                        string[] celdas = linea.Split('\t');
+
+                        for (int indice = 0; indice < celdas.GetLength(0); ++indice)
+                        {
+                            if (columna + indice < datagrid.ColumnCount)
+                            {
+                                objeto_celda = datagrid[columna + indice, fila];
+
+                                //Mientras celda sea Diferente de ReadOnly
+                                if (!objeto_celda.ReadOnly)
+                                {
+                                    if (objeto_celda.Value.ToString() != celdas[indice])
+                                    {
+                                        objeto_celda.Value = Convert.ChangeType(celdas[indice], objeto_celda.ValueType);
+                                        //A continuación la linea añadida para eliminar los '\r'. De paso, y por si acaso en algún contexto ocurre, tambien los: '\t' y '\n'
+                                        objeto_celda.Value = objeto_celda.Value.ToString().Trim(new Char[] { '\t', '\n', '\r' });
+                                        // Fin linea añadida
+                                    }
+                                }
+                                else
+                                {
+                                    // solo intercepta un error si los datos que está pegando es en una celda de solo lectura.
+                                    error++;
+                                }
+                            }
+                            else
+                            { break; }
+                        }
+                        fila++;
+                    }
+                    else
+                    { break; }
+
+                    if (error > 0)
+                        MessageBox.Show(string.Format("{0}  La celda no puede ser actualizada, debido a que la configuración de la columna es de solo lectura.", error),
+                                                  "ADVERTENCIA", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (FormatException fexcepcion)
+            {
+                MessageBox.Show("Los datos que pegó están en el formato incorrecto para la celda." + "\n\nDETALLES: \n\n" + fexcepcion.Message,
+                                "ADVERTENCIA", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+        }
+
+
+
+
+
+        private void Agregar_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Add("Col_Num", "Num");
+            dataGridView1.Columns.Add("Col_Nombre", "Nombre");
+            dataGridView1.Columns["Col_Nombre"].Width = 150;
+            dataGridView1.Columns.Add("Col_Fecha", "Fecha");
+            dataGridView1.Columns.Add("Col_Turno", "Turno");
+            dataGridView1.Columns.Add("Col_Valor", "Valor");
+        }
+
+        private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        private void datagrid_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                // Si el mouse se mueve fuera del rectángulo, comience a arrastrar.
+                if (dragBoxFromMouseDown != Rectangle.Empty &&
+                    !dragBoxFromMouseDown.Contains(e.X, e.Y))
+                {
+
+                    // Continúe arrastrando y soltando, pasando el elemento de la lista.                   
+                    DragDropEffects dropEffect = dataGridView1.DoDragDrop(
+                    dataGridView1.Rows[rowIndexFromMouseDown],
+                    DragDropEffects.Move);
+                }
+            }
+        }
+
+        private void datagrid_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Obtenga el índice del elemento que se encuentra debajo del mouse.
+            rowIndexFromMouseDown = dataGridView1.HitTest(e.X, e.Y).RowIndex;
+            if (rowIndexFromMouseDown != -1)
+            {
+                // Remember the point where the mouse down occurred. 
+                // The DragSize indicates the size that the mouse can move 
+                // before a drag event should be started.                
+                Size dragSize = SystemInformation.DragSize;
+
+                // Create a rectangle using the DragSize, with the mouse position being
+                // at the center of the rectangle.
+                dragBoxFromMouseDown = new Rectangle(new Point(e.X - (dragSize.Width / 2),
+                                                               e.Y - (dragSize.Height / 2)),
+                                    dragSize);
+            }
+            else
+                dragBoxFromMouseDown = Rectangle.Empty;
+            // Reset the rectangle if the mouse is not over an item in the ListBox.
+        }
+
+        private void datagrid_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+
+        private void datagrid_DragDrop(object sender, DragEventArgs e)
+        {
+            // The mouse locations are relative to the screen, so they must be 
+            // converted to client coordinates.
+            Point clientPoint = dataGridView1.PointToClient(new Point(e.X, e.Y));
+
+            // Get the row index of the item the mouse is below. 
+            rowIndexOfItemUnderMouseToDrop =
+                dataGridView1.HitTest(clientPoint.X, clientPoint.Y).RowIndex;
+
+            // If the drag operation was a move then remove and insert the row.
+            if (e.Effect == DragDropEffects.Move)
+            {
+                DataGridViewRow rowToMove = e.Data.GetData(
+                    typeof(DataGridViewRow)) as DataGridViewRow;
+                dataGridView1.Rows.RemoveAt(rowIndexFromMouseDown);
+                dataGridView1.Rows.Insert(rowIndexOfItemUnderMouseToDrop, rowToMove);
+
+            }
+        }
+
+        private void rojoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Obtener la numero de fila seleccionada por el usuario (Codigo Encabezado)
+            if(dataGridView1.CurrentRow.Index != null)
+            {
+                int NumFila = -1;
+                NumFila = dataGridView1.CurrentRow.Index;
+
+                if (NumFila >= 0) {
+                    dataGridView1.Rows[NumFila].DefaultCellStyle.BackColor = Color.FromArgb(255, 213, 79);
+                }
+            }
+        }
+
+        private void amarilloToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Obtener la numero de fila seleccionada por el usuario (Codigo Encabezado)
+            if (dataGridView1.CurrentRow.Index != null)
+            {
+                int NumFila = -1;
+                NumFila = dataGridView1.CurrentRow.Index;
+
+                if (NumFila >= 0)
+                {
+                    dataGridView1.Rows[NumFila].DefaultCellStyle.BackColor = Color.FromArgb(23, 89, 123);
+                }
+            }
+        }
+
+        private void azulToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Obtener la numero de fila seleccionada por el usuario (Codigo Encabezado)
+            if (dataGridView1.CurrentRow.Index != null)
+            {
+                int NumFila = -1;
+                NumFila = dataGridView1.CurrentRow.Index;
+
+                if (NumFila >= 0)
+                {
+                    dataGridView1.Rows[NumFila].DefaultCellStyle.BackColor = Color.FromArgb(34, 100, 155);
+                }
+            }
+        }
+
+        private void ningunoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Obtener la numero de fila seleccionada por el usuario (Codigo Encabezado)
+            if (dataGridView1.CurrentRow.Index != null)
+            {
+                int NumFila = -1;
+                NumFila = dataGridView1.CurrentRow.Index;
+
+                if (NumFila >= 0)
+                {
+                    dataGridView1.Rows[NumFila].DefaultCellStyle.BackColor = Color.FromArgb(255, 255, 255);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.AllowDrop = true;
+
+            if (dataGridView1.Rows.Count > 0) {
+                
+                for (int i = 0; i < dataGridView1.Rows.Count; i++) {
+                    if (dataGridView1.Rows[i].DefaultCellStyle.BackColor.ToArgb() != 0 )
+                        dataGridView1.Rows.RemoveAt(i);
+                    
+                    //MessageBox.Show(dataGridView1.Rows[i].DefaultCellStyle.BackColor.ToArgb().ToString());   
+                }
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            dataGridView1.AllowDrop = false;
+
+            if (dataGridView1.Rows.Count > 0)
+            {
+
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    if (dataGridView1.Rows[i].Cells["Col_Num"].Value.ToString() == "")
+                        dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.FromArgb(23, 89, 123);
+
+                }
+            }
+        }
+    }
+}
